2b0b3c0c186efb82bf6fcb1df4372b6d7300c126
yuniso <soyyn2206@gmail.com>
2022-10-11 23:08:10 +0900

    Error

---
+++ src/components/units/main/main.presenter.tsx
@@ -0,0 +1,36 @@
+import * as S from "./main.style";
+
+export default function MainUI(props: any) {
+  const Items = props.data?.fetchUseditems.splice(8, 2);
+
+  return (
+    <S.Wrapper>
+      <S.Title>New Arrival</S.Title>
+      <S.ItemCardWrapper>
+        {props.data?.fetchUseditems.map((e: any) => (
+          <S.ItemCard
+            key={e._id}
+            id={e._id}
+            onClick={props.onClickMoveToDetail}
+          >
+            <S.ItemImage
+              src={
+                e.images[0]
+                  ? `https://storage.googleapis.com/${e.images[0]}`
+                  : "/main/main.png"
+              }
+            />
+            <S.ItemDetail>
+              <S.TagsDetail>
+                <S.Tags>{e.tags[0] ? e.tags : "기본태그"}</S.Tags>
+                <S.Price>{e.price.toLocaleString()}</S.Price>
+              </S.TagsDetail>
+              <S.seller>{e.seller.name}</S.seller>
+              <S.name>{e.name}</S.name>
+            </S.ItemDetail>
+          </S.ItemCard>
+        ))}
+      </S.ItemCardWrapper>
+    </S.Wrapper>
+  );
+}
