5539d27d785305a778b36115cf33db098ec131ab
yuniso <soyun2206@gmail.com>
2022-10-22 13:06:23 +0900

    [Feat] writeUI

--- src/components/units/market/write/marketWrite.presenter.tsx
+++ src/components/units/market/write/marketWrite.presenter.tsx
@@ -1,3 +1,55 @@
+import * as S from "./marketWrite.styles";
+
 export default function MarketWriteUI() {
-  return <div>안녕하세요</div>;
+  return (
+    <S.Wrapper>
+      <S.WriteTitle>상품 등록</S.WriteTitle>
+      <S.Line />
+      <S.InputWrapper>
+        <S.Label>상품명</S.Label>
+        <S.Name placeholder="상품명을 작성해주세요" />
+      </S.InputWrapper>
+      <S.InputLine2 />
+      <S.InputWrapper>
+        <S.Label>상품 요약</S.Label>
+        <S.Remarks placeholder="상품요약을 작성해주세요" />
+      </S.InputWrapper>
+      <S.InputLine2 />
+      <S.InputWrapper>
+        <S.Label>상품내용</S.Label>
+        <S.Contents placeholder="상품을 설명해주세요." />
+      </S.InputWrapper>
+      <S.InputLine2 />
+      <S.InputWrapper>
+        <S.Label>판매 가격</S.Label>
+        <S.Price placeholder="판매 가격을 입력해주세요" />
+      </S.InputWrapper>
+      <S.InputLine2 />
+      <S.InputWrapper>
+        <S.Label>태그 입력</S.Label>
+        <S.Tags placeholder="#태그 #태그 #태그" />
+      </S.InputWrapper>
+      <S.InputLine2 />
+      <S.AddressWrapper>
+        <S.Label>브랜드 위치</S.Label>
+        <S.PostalCodeWrapper>
+          <S.PostalCode placeholder="07250" />
+          <S.PostalCodeButton>우편번호 검색</S.PostalCodeButton>
+        </S.PostalCodeWrapper>
+        <S.AddressDetail>
+          <S.Address1 />
+          <S.Address2 />
+        </S.AddressDetail>
+      </S.AddressWrapper>
+      <S.InputLine2 />
+      <S.UploadWrapper>
+        <S.Label>사진 첨부</S.Label>
+      </S.UploadWrapper>
+      <S.Line />
+      <S.ButtonWrapper>
+        <S.CancelButton>취소</S.CancelButton>
+        <S.WriteButton>등록</S.WriteButton>
+      </S.ButtonWrapper>
+    </S.Wrapper>
+  );
 }
---
+++ src/components/units/market/write/marketWrite.styles.ts
@@ -0,0 +1,143 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 1920px;
+  padding: 104px 0px 113px 91px;
+`;
+export const WriteTitle = styled.div`
+  font-size: 40px;
+  font-weight: 700;
+  color: #555555;
+  margin-left: 37px;
+`;
+export const Line = styled.div`
+  width: 1737.5px;
+  border: 3px solid #555555;
+  background-color: #555555;
+  margin-top: 31px;
+`;
+export const InputWrapper = styled.div`
+  display: flex;
+  margin-top: 34px;
+  align-items: center;
+`;
+export const Label = styled.div`
+  font-size: 24px;
+  font-weight: 500;
+  margin-left: 43px;
+`;
+export const Name = styled.input`
+  width: 1419px;
+  height: 56px;
+  background-color: #e9e9e9;
+  border: none;
+  outline: none;
+  font-size: 24px;
+  margin-left: 195px;
+  padding-left: 19px;
+
+  ::placeholder {
+    font-size: 15px;
+    color: #a9a9a9;
+    padding-left: 19px;
+  }
+`;
+export const InputLine2 = styled.div`
+  width: 1735.5px;
+  border: 1px solid #bdbdbd;
+  margin-top: 26px;
+`;
+export const Remarks = styled.input`
+  width: 1419px;
+  height: 56px;
+  background-color: #e9e9e9;
+  border: none;
+  outline: none;
+  font-size: 24px;
+  margin-left: 165px;
+  padding-left: 19px;
+
+  ::placeholder {
+    font-size: 15px;
+    color: #a9a9a9;
+    padding-left: 19px;
+  }
+`;
+export const Contents = styled.input`
+  width: 1419px;
+  height: 56px;
+  background-color: #e9e9e9;
+  border: none;
+  outline: none;
+  font-size: 24px;
+  margin-left: 171px;
+  padding-left: 19px;
+
+  ::placeholder {
+    font-size: 15px;
+    color: #a9a9a9;
+    padding-left: 19px;
+  }
+`;
+export const Price = styled.input`
+  width: 1419px;
+  height: 56px;
+  background-color: #e9e9e9;
+  border: none;
+  outline: none;
+  font-size: 24px;
+  margin-left: 163px;
+  padding-left: 19px;
+
+  ::placeholder {
+    font-size: 15px;
+    color: #a9a9a9;
+    padding-left: 19px;
+  }
+`;
+export const Tags = styled.input`
+  width: 1419px;
+  height: 56px;
+  background-color: #e9e9e9;
+  border: none;
+  outline: none;
+  font-size: 24px;
+  margin-left: 162px;
+  padding-left: 19px;
+
+  ::placeholder {
+    font-size: 15px;
+    color: #a9a9a9;
+    padding-left: 19px;
+  }
+`;
+export const AddressWrapper = styled.div``;
+export const PostalCodeWrapper = styled.div``;
+export const PostalCode = styled.input``;
+export const PostalCodeButton = styled.button``;
+export const AddressDetail = styled.div``;
+export const Address1 = styled.input``;
+export const Address2 = styled.input``;
+export const UploadWrapper = styled.div``;
+export const ButtonWrapper = styled.div`
+  padding: 73px 746px 0px 677px;
+`;
+export const CancelButton = styled.button`
+  width: 195px;
+  height: 77px;
+  background-color: #ffffff;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+`;
+export const WriteButton = styled.button`
+  width: 195px;
+  height: 77px;
+  background-color: #555555;
+  margin-left: 16px;
+  font-size: 20px;
+  font-weight: 700;
+  color: #ffffff;
+  border: none;
+  cursor: pointer;
+`;
